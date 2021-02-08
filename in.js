0698652c3098c8fe79664f218a9022ae24395caf
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-08 19:37:51 +0530

    Create in.js

---
+++ in.js
@@ -0,0 +1,11 @@
+let c = [1, 2];
+let d = [1, 2, 3];
+let e = [
+    function foo() {
+        dosomething();
+    }, function bar() {
+        dosomething();
+    }
+];
+
+var foo = Boolean(!!bar);
