e48ff7aefcef46732559d08fbcb6577c039af24f
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-11 20:59:33 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,15 @@ switch(foo){
    case g: foo(); break ;
    default : break;
 }
+
+let c = [1, 2];
+let d = [1, 2, 3];
+let e = [
+    function foo() {
+        dosomething();
+    }, function bar() {
+        dosomething();
+    }
+];
+
+var foo = Boolean(!!bar);
