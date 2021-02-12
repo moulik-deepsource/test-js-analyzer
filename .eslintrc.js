f9f29fa458d5ae63b3ec2e1eced4d7b922b3829f
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-12 22:41:05 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-      parser: "@typescript-eslint/parser".
+      parser: "@typescript-eslint/parser",
       rules: {
          "indent": ["error", 2, { "SwitchCase": 1 }]
    }
