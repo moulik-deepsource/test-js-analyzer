91fd36651d60bf4e55b55170490f41d1ba64bfae
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-24 20:29:55 +0530

    Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-      parser: "@typescript-eslint/parser",
-      rules: {
-         "indent": ["error", 2, { "SwitchCase": 1 }],
-         "yoda"  : ["error", "always"]
-   }
-}
