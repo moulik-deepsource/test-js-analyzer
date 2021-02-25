470746bd583433780fda2fe3ef41947472b95d8d
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-25 15:33:56 +0530

    Delete .eslintrc.js

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-      parser: "@typescript-eslint/parser",
-      plugins: ['react'],
-      rules: {
-         "indent": ["error", 2, { "SwitchCase": 1 }],
-         "react/jsx-sort-props"  : "error"
-   }
-}
