238d1054a6f6d0a71ba44898a698f1d1f2fdfe6c
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-25 12:35:14 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
       parser: "@typescript-eslint/parser",
+      plugins: ['react'],
       rules: {
          "indent": ["error", 2, { "SwitchCase": 1 }],
          "react/jsx-sort-props"  : "error"
