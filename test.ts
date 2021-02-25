22708439177b8b1c60bd70d6dafcdec017648510
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-25 11:58:04 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
+import foo from 'foo';
+import bar from 'foo';
+
 type T = { x: number; };
+
+f.b();
+
+
+function foo() {
+  let a = 1,
+    b;
+  if (a) {
+    let c = 3,
+      d = null;
+  }
+}
