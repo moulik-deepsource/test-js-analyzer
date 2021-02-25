1f655ef6e760cc95f3c5ba9362e52a5c2cc3f8f1
aniketh-deepsource <76031695+aniketh-deepsource@users.noreply.github.com>
2021-02-25 12:04:13 +0530

    Update test2.ts

--- test2.ts
+++ test2.ts
@@ -1,3 +1,25 @@
 type x = { f: number }
 
 var obj = {'foo': 'bar'};
+
+type T =
+  | [1, 2, 4]
+  | [1, 2, 3]
+  | { b: string }
+  | { a: string }
+  | (() => void)
+  | (() => string)
+  | "b"
+  | "a"
+  | 'b'
+  | 'a'
+  | readonly string[]
+  | readonly number[]
+  | string[]
+  | number[]
+  | B
+  | A
+  | undefined
+  | null
+  | string
+  | any;
